2caafa575a821e4c9068278b6c25381b7bd9fe43
OldManLink <oldmanlink@gmail.com>
2013-07-02 10:57:45 +0200

    Restructuring
    Made the structure closer to that of the builder project

---
+++ src/main/java/com/diabol/pipefitter/PipelineBuilder.java
@@ -0,0 +1,11 @@
+package com.diabol.pipefitter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: peter
+ * Date: 2013-06-13
+ * Time: 16:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class PipelineBuilder {
+}
