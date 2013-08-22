814439d59a28a4a74591932bbc275f296c8d4812
Patrik Boström <patbos@patbos.com>
2013-08-22 12:44:49 +0200

    Property

--- src/main/java/com/diabol/pipefitter/PipelineView.java
+++ src/main/java/com/diabol/pipefitter/PipelineView.java
@@ -95,6 +95,7 @@ public class PipelineView extends View {
 
         List<Stage> stages = newArrayList();
         for (AbstractProject job : getAllDownstreamJobs(first)) {
+            PipelinePlugin.PipelineProperty property = (PipelinePlugin.PipelineProperty) job.getProperty(PipelinePlugin.PipelineProperty.class);
             AbstractBuild build = job.getLastBuild();
             Task task;
             if (stages.isEmpty() || build != null && build.equals(getDownstreamBuild(job, prevBuild))) {
