10fd1387b046a742b3a9a059a7cef3c2531d3b48
Patrik Boström <patbos@patbos.com>
2013-08-26 17:16:11 +0200

    Fixed bug when we could not resolve job status

--- src/main/java/se/diabol/pipefitter/PipelineFactory.java
+++ src/main/java/se/diabol/pipefitter/PipelineFactory.java
@@ -24,10 +24,9 @@ import static java.util.Collections.singleton;
 /**
  * @author Per Huss <mr.per.huss@gmail.com>
  */
-public class PipelineFactory
-{
-    public Pipeline extractPipeline(String name, AbstractProject<?, ?> firstJob)
-    {
+public class PipelineFactory {
+
+    public Pipeline extractPipeline(String name, AbstractProject<?, ?> firstJob) {
         Map<String, Stage> stages = newLinkedHashMap();
         for (AbstractProject job : getAllDownstreamJobs(firstJob).values()) {
             PipelineProperty property = (PipelineProperty) job.getProperty(PipelineProperty.class);
@@ -44,8 +43,7 @@ public class PipelineFactory
         return new Pipeline(name, newArrayList(stages.values()));
     }
 
-    private Map<String, AbstractProject> getAllDownstreamJobs(AbstractProject first)
-    {
+    private Map<String, AbstractProject> getAllDownstreamJobs(AbstractProject first) {
         Map<String, AbstractProject> jobs = newLinkedHashMap();
         jobs.put(first.getName(), first);
         for (AbstractProject project : getDownstreamProjects(first))
@@ -53,32 +51,32 @@ public class PipelineFactory
         return jobs;
     }
 
-    /** Opens up for testing and mocking, since Jenkins has getDownstreamProjects() final */
-    List<AbstractProject<?, ?>> getDownstreamProjects(AbstractProject project)
-    {
+    /**
+     * Opens up for testing and mocking, since Jenkins has getDownstreamProjects() final
+     */
+    List<AbstractProject<?, ?>> getDownstreamProjects(AbstractProject project) {
         //noinspection unchecked
         return project.getDownstreamProjects();
     }
 
 
     public Pipeline createPipelineLatest(Pipeline pipeline) {
-        AbstractBuild prevBuild = null;
-        List<AbstractBuild> builds = new ArrayList<>();
         List<Stage> stages = new ArrayList<>();
+        AbstractBuild firstBuild = null;
         for (Stage stage : pipeline.getStages()) {
             List<Task> tasks = new ArrayList<>();
             for (Task task : stage.getTasks()) {
                 AbstractProject job = Jenkins.getInstance().getItem(task.getId().toString(), Jenkins.getInstance(), AbstractProject.class);
                 AbstractBuild build = job.getLastBuild();
-                //Map<AbstractProject, Integer> hepp = build.getUpstreamBuilds();
-                if (stages.isEmpty() || build != null && build.equals(getDownstreamBuild(job, prevBuild))) {
+                if (firstBuild == null) {
+                    firstBuild = build;
+                }
+                if (build != null && firstBuild.equals(getFirstUpstreamBuild(build))) {
                     Status status = resolveStatus(build);
                     tasks.add(new Task(task.getId(), task.getName(), status));
                 } else {
                     tasks.add(new Task(task.getId(), task.getName(), StatusFactory.idle()));
                 }
-
-                prevBuild = build;
 
             }
             stages.add(new Stage(stage.getName(), tasks));
@@ -88,8 +86,7 @@ public class PipelineFactory
     }
 
 
-    private static Status resolveStatus(AbstractBuild build)
-    {
+    private static Status resolveStatus(AbstractBuild build) {
         if (build.isBuilding()) {
             return StatusFactory.running((int) round(100.0d * (currentTimeMillis() - build.getTimestamp().getTimeInMillis())
                     / build.getEstimatedDuration()));
@@ -107,47 +104,26 @@ public class PipelineFactory
         throw new IllegalStateException("Result " + result + " not recognized.");
     }
 
-    public static AbstractBuild<?, ?> getDownstreamBuild(final AbstractProject<?, ?> downstreamProject,
-                                                         final AbstractBuild<?, ?> upstreamBuild) {
-        if ((downstreamProject != null) && (upstreamBuild != null)) {
-            final List<AbstractBuild<?, ?>> downstreamBuilds = (List<AbstractBuild<?, ?>>) downstreamProject.getBuilds();
-            for (final AbstractBuild<?, ?> innerBuild : downstreamBuilds) {
-                for (final CauseAction action : innerBuild.getActions(CauseAction.class)) {
-                    for (final Cause cause : action.getCauses()) {
-                        if (cause instanceof Cause.UpstreamCause) {
-                            final Cause.UpstreamCause upstreamCause = (Cause.UpstreamCause) cause;
-                            if (upstreamCause.getUpstreamProject().equals(upstreamBuild.getProject().getFullName())
-                                    && (upstreamCause.getUpstreamBuild() == upstreamBuild.getNumber())) {
-                                return innerBuild;
-                            }
-                        }
-                    }
+    /**
+     * Finds the first upstream job in the chain of triggered jobs.
+     *
+     * @param build the build to find the first upstream for
+     * @return the first upstream build for the given build
+     */
+    private static AbstractBuild getFirstUpstreamBuild(AbstractBuild build) {
+        List<CauseAction> actions = build.getActions(CauseAction.class);
+        for (CauseAction action : actions) {
+            List<Cause> causes = action.getCauses();
+            for (Cause cause : causes) {
+                if (cause instanceof Cause.UpstreamCause) {
+                    Cause.UpstreamCause upstreamCause = (Cause.UpstreamCause) cause;
+                    AbstractProject upstreamProject = Jenkins.getInstance().getItem(upstreamCause.getUpstreamProject(), Jenkins.getInstance(), AbstractProject.class);
+                    AbstractBuild upstreamBuild = upstreamProject.getBuildByNumber(upstreamCause.getUpstreamBuild());
+                    return getFirstUpstreamBuild(upstreamBuild);
                 }
             }
         }
-        return null;
+        return build;
     }
 
-/*   public static boolean isDownstreamBuildFor(AbstractBuild firstBuild, AbstractBuild build) {
-        List<AbstractProject> upStreamProjects = build.getProject().getUpstreamProjects();
-        for (int i = 0; i < upStreamProjects.size(); i++) {
-            AbstractProject project = upStreamProjects.get(i);
-            List<CauseAction> actions = project.getLastBuild().getActions(CauseAction.class);
-            for (int j = 0; j < actions.size(); j++) {
-                CauseAction causeAction = actions.get(j);
-                List<Cause> causes = causeAction.getCauses();
-                for (int k = 0; k < causes.size(); k++) {
-                    Cause cause = causes.get(k);
-                    if (cause instanceof Cause.UpstreamCause) {
-                        Cause.UpstreamCause upstreamCause = (Cause.UpstreamCause) cause;
-                        if ()
-                    }
-                }
-            }
-        }
-   }*/
-
-
-
-//    }
 }
