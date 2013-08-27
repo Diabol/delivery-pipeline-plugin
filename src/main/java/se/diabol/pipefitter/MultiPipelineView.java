54cb9fa8ae3be59020b6c189abac4b8a2c367120
Patrik Boström <patbos@patbos.com>
2013-08-27 13:46:08 +0200

    MultiViews now showing

--- src/main/java/se/diabol/pipefitter/MultiPipelineView.java
+++ src/main/java/se/diabol/pipefitter/MultiPipelineView.java
@@ -1,11 +1,14 @@
 package se.diabol.pipefitter;
 
 import hudson.Extension;
-import hudson.model.AbstractDescribableImpl;
-import hudson.model.Descriptor;
-import hudson.model.ViewDescriptor;
+import hudson.model.*;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
+import se.diabol.pipefitter.model.Pipeline;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultiPipelineView extends AbstractPipelineView {
@@ -13,7 +16,7 @@ public class MultiPipelineView extends AbstractPipelineView {
     private List<Component> components;
 
     @DataBoundConstructor
-    public MultiPipelineView(String name,List<Component> components ) {
+    public MultiPipelineView(String name, List<Component> components ) {
         super(name);
         this.components = components;
     }
@@ -26,6 +29,21 @@ public class MultiPipelineView extends AbstractPipelineView {
         this.components = components;
     }
 
+
+    public List<Pipeline> getPipelines()
+    {
+        PipelineFactory pipelineFactory = new PipelineFactory();
+        List<Pipeline> result = new ArrayList<>();
+        for (Component component : components) {
+            AbstractProject firstJob = Jenkins.getInstance().getItem(component.getFirstJob(), Jenkins.getInstance(), AbstractProject.class);
+
+            result.add(pipelineFactory.createPipelineLatest(pipelineFactory.extractPipeline(component.getName(), firstJob)));
+
+        }
+        return result;
+    }
+
+
     @Extension
     public static class DescriptorImpl extends ViewDescriptor {
         public String getDisplayName() {
@@ -36,15 +54,21 @@ public class MultiPipelineView extends AbstractPipelineView {
 
 
     public static class Component extends AbstractDescribableImpl<Component> {
-        public String name;
+        private String name;
+        private String firstJob;
 
         @DataBoundConstructor
-        public Component(String name) {
+        public Component(String name, String firstJob) {
             this.name = name;
+            this.firstJob = firstJob;
         }
 
         public String getName() {
             return name;
+        }
+
+        public String getFirstJob() {
+            return firstJob;
         }
 
         @Extension
@@ -52,6 +76,14 @@ public class MultiPipelineView extends AbstractPipelineView {
             @Override
             public String getDisplayName() {
                 return "";
+            }
+
+            public ListBoxModel doFillFirstJobItems(@AncestorInPath ItemGroup<?> context) {
+                final hudson.util.ListBoxModel options = new hudson.util.ListBoxModel();
+                for (final AbstractProject<?, ?> p : Jenkins.getInstance().getAllItems(AbstractProject.class)) {
+                    options.add(p.getFullDisplayName(), p.getRelativeNameFrom(context));
+                }
+                return options;
             }
 
 
