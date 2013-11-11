a3253da4553206535f80f9dae03ee0d1fb611d82
Patrik Boström <patbos@patbos.com>
2013-11-11 13:37:55 +0100

    Added test for NameComparator

---
+++ src/test/java/se/diabol/jenkins/pipeline/sort/NameComparatorTest.java
@@ -0,0 +1,44 @@
+/*
+This file is part of Delivery Pipeline Plugin.
+
+Delivery Pipeline Plugin is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Delivery Pipeline Plugin is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Delivery Pipeline Plugin.
+If not, see <http://www.gnu.org/licenses/>.
+*/
+package se.diabol.jenkins.pipeline.sort;
+
+import org.junit.Test;
+import se.diabol.jenkins.pipeline.model.Component;
+import se.diabol.jenkins.pipeline.model.Pipeline;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class NameComparatorTest {
+
+    @Test
+    public void testCompare() {
+        Component componentB = new Component("B", new ArrayList<Pipeline>());
+        Component componentA = new Component("A", new ArrayList<Pipeline>());
+        List<Component> list = new ArrayList<Component>();
+        list.add(componentB);
+        list.add(componentA);
+        Collections.sort(list, new NameComparator());
+        assertEquals(componentA, list.get(0));
+        assertEquals(componentB, list.get(1));
+    }
+
+}
