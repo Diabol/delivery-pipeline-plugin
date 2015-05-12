00aaf7f4f15773c766e70e75cedea0c5ebf9e3ac
Patrik Boström <patbos@patbos.com>
2015-05-12 20:11:01 +0200

    Fix for strange case that I cant see will happen and cant create a test that covers

--- src/main/java/se/diabol/jenkins/pipeline/DeliveryPipelineView.java
+++ src/main/java/se/diabol/jenkins/pipeline/DeliveryPipelineView.java
@@ -426,19 +426,9 @@ public class DeliveryPipelineView extends View {
         
         if (!isDefault()) {
             return getOwner().getPrimaryView().doCreateItem(req, rsp);
+        } else {
+            return Jenkins.getInstance().doCreateItem(req, rsp);
         }
-        
-        // when we are the default view, dynamically load the "All" view
-        // and defer to the "All" view's implemenation for creating a new item
-        try {
-            allView = new hudson.model.AllView("All" , getOwner());
-        } catch(Exception e) {
-            // return null in the unlikely event that the AllView class
-            // changes its constructor signatures, which would raise a NoSuchMethodException
-            return null;
-        }
-        
-        return allView.doCreateItem(req, rsp);
     }
 
 
