52c7cace7e8aaf6551169ada7c31b5d2d2111543
Tommy Tynjä <tommy.tynja@diabol.se>
2016-11-12 16:18:09 +0100

    JENKINS-34040 Increased test coverage for Task model class

--- src/test/java/se/diabol/jenkins/workflow/model/TaskTest.java
+++ src/test/java/se/diabol/jenkins/workflow/model/TaskTest.java
@@ -17,13 +17,92 @@ If not, see <http://www.gnu.org/licenses/>.
 */
 package se.diabol.jenkins.workflow.model;
 
+import com.google.api.client.util.DateTime;
+import org.jenkinsci.plugins.workflow.actions.TimingAction;
+import org.jenkinsci.plugins.workflow.graph.FlowNode;
 import org.junit.Test;
 
-import static java.lang.Math.round;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import se.diabol.jenkins.workflow.api.Stage;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TaskTest {
 
+    private final static Long expectedStageDuration = 1000L;
+
     @Test
-    public void shouldCalculateProgress() {
+    public void isRunningShouldHandleNullParameter() {
+        assertThat(Task.isRunning(null), is(false));
+    }
+
+    @Test
+    public void isRunningShouldHandleEmptyParameter() {
+        assertThat(Task.isRunning(Collections.<FlowNode>emptyList()), is(false));
+    }
+
+    @Test
+    public void shouldGetStartTime() {
+        long expectedStartTime = 123L;
+        List<FlowNode> flowNodes = new ArrayList<FlowNode>(1);
+        FlowNode flowNode = mock(FlowNode.class);
+        TimingAction timingAction = mock(TimingAction.class);
+        when(timingAction.getStartTime()).thenReturn(expectedStartTime);
+        when(flowNode.getAction(TimingAction.class)).thenReturn(timingAction);
+        flowNodes.add(flowNode);
+
+        assertThat(Task.getStartTime(flowNodes), is(expectedStartTime));
+    }
+
+    @Test
+    public void shouldGetStartTimeForNullParameter() {
+        assertThat(Task.getStartTime(null), is(0L));
+    }
+
+    @Test
+    public void shouldGetStartTimeForEmptyParameter() {
+        assertThat(Task.getStartTime(Collections.<FlowNode>emptyList()), is(0L));
+    }
+
+    @Test
+    public void shouldGetDuration() {
+        List<Stage> stages = stages();
+        long duration = Task.getDuration(stages);
+        assertThat(duration, is(expectedStageDuration * stages.size()));
+    }
+
+    @Test
+    public void shouldGetDurationForNullParameter() {
+        long duration = Task.getDuration(null);
+        assertThat(duration, is(0L));
+    }
+
+    @Test
+    public void shouldGetDurationForEmptyParameter() {
+        long duration = Task.getDuration(Collections.<Stage>emptyList());
+        assertThat(duration, is(0L));
+    }
+
+    private static List<Stage> stages() {
+        List<Stage> stages = new ArrayList<Stage>(5);
+        for (int i = 0; i < 5; i++) {
+            stages.add(stage("stage" + i));
+        }
+        return stages;
+    }
+
+    private static Stage stage(String name) {
+        return new Stage(null,
+                name,
+                name,
+                "SUCCESS",
+                new DateTime(0L),
+                expectedStageDuration);
     }
 }
