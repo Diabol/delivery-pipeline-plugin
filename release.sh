3d03a837dec016320f00662be890b62239b681c6
Andreas Rehn <mrfatstrat@gmail.com>
2016-12-01 12:18:16 +0100

    fixes in release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-#set -x
+#!/bin/bash
+set -x
 if [ $# -eq 2 ]
   then
     branch=$1
@@ -9,12 +9,12 @@ if [ $# -eq 2 ]
     echo "Usage: release.sh <branch> <tag>"
 fi
 
-if [ -n "$tag" ] && [[ $tag =~ ^release-.* ]]; then
+if [[ -n "$tag" ]] && [[ $tag =~ ^release-.* ]]; then
   version=`echo $tag | sed -n 's/release-\(.\)/\1/p'`
   git checkout -qf $branch
   head=`git rev-parse HEAD`
-  commit0`git rev-list -n 1 $tag`
-  if [ $head == $commit ]; then
+  commit=`git rev-list -n 1 $tag`
+  if [[ $head -eq $commit ]]; then
     mvn --settings settings.xml --batch-mode -DdryRun=true -DreleaseVersion=$version -Darguments="-DskipTests=true" -DskipTests=true release:prepare release:perform
   else
     echo "Tag $tag does not point to $branch/head, cannot release"
