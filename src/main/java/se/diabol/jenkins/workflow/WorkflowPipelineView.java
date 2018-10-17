63871c3ab98dba82c8637ec2eb2af9e6bb47b574
Tommy Tynjä <tommy.tynja@diabol.se>
2018-10-17 08:41:40 +0200

    JENKINS-44628 Removed unused import

--- src/main/java/se/diabol/jenkins/workflow/WorkflowPipelineView.java
+++ src/main/java/se/diabol/jenkins/workflow/WorkflowPipelineView.java
@@ -65,7 +65,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nonnull;
