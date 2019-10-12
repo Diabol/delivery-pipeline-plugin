db1afd1e98c4b40a1c2e952f91f2fca196a52065
Sean Ly <seanly@aliyun.com>
2019-10-12 19:22:15 +0800

    add PipelineVersionBuildVariableContributor

---
+++ src/main/java/se/diabol/jenkins/pipeline/PipelineVersionBuildVariableContributor.java
@@ -0,0 +1,21 @@
+package se.diabol.jenkins.pipeline;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildVariableContributor;
+import se.diabol.jenkins.pipeline.PipelineVersionContributor.PipelineVersionAction;
+
+import java.util.Map;
+
+@Extension
+public class PipelineVersionBuildVariableContributor extends BuildVariableContributor {
+    @Override
+    public void buildVariablesFor(AbstractBuild build, Map<String, String> variablesOut) {
+        PipelineVersionAction pipelineVersionAction = build.getAction(PipelineVersionAction.class);
+
+        if (pipelineVersionAction != null) {
+            variablesOut.put(PipelineVersionContributor.VERSION_PARAMETER, pipelineVersionAction.getVersion());
+        }
+
+    }
+}
